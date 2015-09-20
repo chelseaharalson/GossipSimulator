c2faad703af18f43849894142d24aa050428f2ba
Chelsea Metcalf <chelseametcalf@ufl.edu>
2015-09-20 13:13:26 -0400

    Gossip implemented

--- project2/src/main/scala/project2/Worker.scala
+++ project2/src/main/scala/project2/Worker.scala
@@ -9,18 +9,22 @@ import scala.util.Random
 class Worker(idx: Int, numOfNodes: Int, top: Int, alg: Int) extends Actor {
 
   var numOfMessages: Int = 0
+  var gossipTermination: Int = 10
 
   def receive = {
     // Gossip Rumor
     case Rumor(message) => {
-      println("Message: " + message)
-      val t = new Topology()
-      t.numOfNodes = numOfNodes
-      t.topType = top
-      t.idx = idx
-      val nextNode = t.findNode()
-      //println(t.findNode())
-      context.actorSelection("../" + nextNode.toString()) ! Rumor(message)
+      numOfMessages = numOfMessages + 1
+      if (numOfMessages <= gossipTermination) {
+        println("Index: " + idx + " Message: " + message + " Num of Messages: " + numOfMessages)
+        val t = new Topology()
+        t.numOfNodes = numOfNodes
+        t.topType = top
+        t.idx = idx
+        val nextNode = t.findNode()
+        //println(t.findNode())
+        context.actorSelection("../" + nextNode.toString()) ! Rumor(message)
+      }
     }
     case msg: String => {
       println(msg)
